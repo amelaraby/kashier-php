ad907d27eccfa543508dc5da6dcda68ac77ae626
Ahmed Elaraby <araby2305@gmail.com>
2019-09-23 00:45:23 +0200

    First release

---
+++ src/Core/KashierConstants.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace ITeam\Kashier\Core;
+
+/**
+ * Class KashierConstants
+ * Placeholder for Kashier Constants
+ *
+ * @package ITeam\Kashier\Core
+ */
+class KashierConstants
+{
+    const SDK_NAME = 'Kashier-PHP-SDK';
+    const SDK_VERSION = '1.0.0';
+
+    const REST_SANDBOX_ENDPOINT = 'https://test-iframe.payformance.io';
+    const REST_LIVE_ENDPOINT = 'https://iframe.payformance.io';
+    const URL_PATH_CHECKOUT = '/checkout';
+    const URL_PATH_TOKENIZATION = '/tokenization';
+}
